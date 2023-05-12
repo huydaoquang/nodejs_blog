4d16b6b53eee3ac7df9e7ea416d0f1a500c6e55a
huy dao <you@example.com>
2023-05-12 10:50:21 +0700

    added [CRUD] Update course

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newsRoutes = require("./news");
 const coursesRoutes = require("./courses");
 const siteRoutes = require("./site");
+const meRoutes = require("./me");
 
 function route(app) {
 	app.use("/news", newsRoutes);
+	app.use("/me", meRoutes);
 	app.use("/courses", coursesRoutes);
 
 	app.use("/", siteRoutes);
